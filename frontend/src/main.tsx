4dbc774fa804eff212e154c2a3be16806e957d0b
Arbaj Ansari <127431775+ArbajAnsari19@users.noreply.github.com>
2024-10-12 23:08:29 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
-import { store } from './utils/store.ts';
+import { store } from '../src/utils/Store.ts';
 import App from './App.tsx'
 import './index.css'
 
